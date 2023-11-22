fc970e69cf7b37b89e06e6ff1e9a88ea53cb17d1
AdilAhmed976 <adilahmedah976@gmail.com>
2023-11-23 01:48:20 +0530

    filters options Data Added

--- src/Common/global.js
+++ src/Common/global.js
@@ -7,180 +7,693 @@ export const sidebarMenus = [
 ];
 
 export const countriesData = [
-  "United States of America",
-  "Mexico",
-  "Nigeria",
-  "Lebanon",
-  "Russia",
-  "Saudi Arabia",
-  "Angola",
-  "Egypt",
-  "South Africa",
-  "India",
-  "Ukraine",
-  "Azerbaijan",
-  "China",
-  "Colombia",
-  "Niger",
-  "Libya",
-  "Brazil",
-  "Mali",
-  "Indonesia",
-  "Iraq",
-  "Iran",
+  {
+    value: "United States of America",
+    label: "United States of America",
+  },
+  {
+    value: "Mexico",
+    label: "Mexico",
+  },
+  {
+    value: "Nigeria",
+    label: "Nigeria",
+  },
+  {
+    value: "Lebanon",
+    label: "Lebanon",
+  },
+  {
+    value: "Russia",
+    label: "Russia",
+  },
+  {
+    value: "Saudi Arabia",
+    label: "Saudi Arabia",
+  },
+  {
+    value: "Angola",
+    label: "Angola",
+  },
+  {
+    value: "Egypt",
+    label: "Egypt",
+  },
+  {
+    value: "South Africa",
+    label: "South Africa",
+  },
+  {
+    value: "India",
+    label: "India",
+  },
+  {
+    value: "Ukraine",
+    label: "Ukraine",
+  },
+  {
+    value: "Azerbaijan",
+    label: "Azerbaijan",
+  },
+  {
+    value: "China",
+    label: "China",
+  },
+  {
+    value: "Colombia",
+    label: "Colombia",
+  },
+  {
+    value: "Niger",
+    label: "Niger",
+  },
+  {
+    value: "Libya",
+    label: "Libya",
+  },
+  {
+    value: "Brazil",
+    label: "Brazil",
+  },
+  {
+    value: "Mali",
+    label: "Mali",
+  },
+  {
+    value: "Indonesia",
+    label: "Indonesia",
+  },
+  {
+    value: "Iraq",
+    label: "Iraq",
+  },
+  {
+    value: "Iran",
+    label: "Iran",
+  },
+  ,
 ];
 
 export const topicsData = [
-  "gas",
-  "oil",
-  "consumption",
-  "market",
-  "gdp",
-  "war",
-  "production",
-  "export",
-  "battery",
-  "biofuel",
-  "policy",
-  "economy",
-  "strategy",
-  "robot",
-  "growth",
-  "economic",
-  "energy",
-  "food",
-  "administration",
-  "unemployment",
-  "trade",
-  "demand",
-  "economic growth",
-  "industry",
-  "capital",
-  "worker",
-  "tension",
-  "terrorism",
-  "transport",
-  "peak oil",
-  "vehicle",
-  "tourist",
-  "artificial intelligence",
-  "climate",
-  "power",
-  "crisis",
-  "ice",
-  "population",
-  "politics",
-  "business",
-  "work",
-  "coal",
-  "gamification",
-  "finance",
-  "interest rate",
-  "risk",
-  "inflation",
-  "asylum",
-  "resource",
-  "plastic",
-  "electricity",
-  "bank",
-  "gasoline",
-  "car",
-  "money",
-  "technology",
-  "aquaculture",
-  "city",
-  "investment",
-  "revenue",
-  "emission",
-  "climate change",
-  "infrastructure",
-  "government",
-  "security",
-  "software",
-  "building",
-  "transportation",
-  "wealth",
-  "clothing",
-  "shortage",
-  "debt",
-  "agriculture",
-  "tax",
-  "carbon",
-  "brexit",
-  "workforce",
-  "change",
-  "automaker",
-  "nuclear",
-  "3D",
-  "water",
-  "data",
-  "fossil fuel",
-  "election",
-  "greenhouse gas",
-  "information",
-  "shale gas",
-  "factory",
-  "farm",
-  "communication",
-  "storm",
-  "consumer",
-  "material",
-  "Washington",
-  "pollution",
-  "fracking",
+  {
+    value: "gas",
+    label: "gas",
+  },
+  {
+    value: "oil",
+    label: "oil",
+  },
+  {
+    value: "consumption",
+    label: "consumption",
+  },
+  {
+    value: "market",
+    label: "market",
+  },
+  {
+    value: "gdp",
+    label: "gdp",
+  },
+  {
+    value: "war",
+    label: "war",
+  },
+  {
+    value: "production",
+    label: "production",
+  },
+  {
+    value: "export",
+    label: "export",
+  },
+  {
+    value: "battery",
+    label: "battery",
+  },
+  {
+    value: "biofuel",
+    label: "biofuel",
+  },
+  {
+    value: "policy",
+    label: "policy",
+  },
+  {
+    value: "economy",
+    label: "economy",
+  },
+  {
+    value: "strategy",
+    label: "strategy",
+  },
+  {
+    value: "robot",
+    label: "robot",
+  },
+  {
+    value: "growth",
+    label: "growth",
+  },
+  {
+    value: "economic",
+    label: "economic",
+  },
+  {
+    value: "energy",
+    label: "energy",
+  },
+  {
+    value: "food",
+    label: "food",
+  },
+  {
+    value: "administration",
+    label: "administration",
+  },
+  {
+    value: "unemployment",
+    label: "unemployment",
+  },
+  {
+    value: "trade",
+    label: "trade",
+  },
+  {
+    value: "demand",
+    label: "demand",
+  },
+  {
+    value: "economic growth",
+    label: "economic growth",
+  },
+  {
+    value: "industry",
+    label: "industry",
+  },
+  {
+    value: "capital",
+    label: "capital",
+  },
+  {
+    value: "worker",
+    label: "worker",
+  },
+  {
+    value: "tension",
+    label: "tension",
+  },
+  {
+    value: "terrorism",
+    label: "terrorism",
+  },
+  {
+    value: "transport",
+    label: "transport",
+  },
+  {
+    value: "peak oil",
+    label: "peak oil",
+  },
+  {
+    value: "vehicle",
+    label: "vehicle",
+  },
+  {
+    value: "tourist",
+    label: "tourist",
+  },
+  {
+    value: "artificial intelligence",
+    label: "artificial intelligence",
+  },
+  {
+    value: "climate",
+    label: "climate",
+  },
+  {
+    value: "power",
+    label: "power",
+  },
+  {
+    value: "crisis",
+    label: "crisis",
+  },
+  {
+    value: "ice",
+    label: "ice",
+  },
+  {
+    value: "population",
+    label: "population",
+  },
+  {
+    value: "politics",
+    label: "politics",
+  },
+  {
+    value: "business",
+    label: "business",
+  },
+  {
+    value: "work",
+    label: "work",
+  },
+  {
+    value: "coal",
+    label: "coal",
+  },
+  {
+    value: "gamification",
+    label: "gamification",
+  },
+  {
+    value: "finance",
+    label: "finance",
+  },
+  {
+    value: "interest rate",
+    label: "interest rate",
+  },
+  {
+    value: "risk",
+    label: "risk",
+  },
+  {
+    value: "inflation",
+    label: "inflation",
+  },
+  {
+    value: "asylum",
+    label: "asylum",
+  },
+  {
+    value: "resource",
+    label: "resource",
+  },
+  {
+    value: "plastic",
+    label: "plastic",
+  },
+  {
+    value: "electricity",
+    label: "electricity",
+  },
+  {
+    value: "bank",
+    label: "bank",
+  },
+  {
+    value: "gasoline",
+    label: "gasoline",
+  },
+  {
+    value: "car",
+    label: "car",
+  },
+  {
+    value: "money",
+    label: "money",
+  },
+  {
+    value: "technology",
+    label: "technology",
+  },
+  {
+    value: "aquaculture",
+    label: "aquaculture",
+  },
+  {
+    value: "city",
+    label: "city",
+  },
+  {
+    value: "investment",
+    label: "investment",
+  },
+  {
+    value: "revenue",
+    label: "revenue",
+  },
+  {
+    value: "emission",
+    label: "emission",
+  },
+  {
+    value: "climate change",
+    label: "climate change",
+  },
+  {
+    value: "infrastructure",
+    label: "infrastructure",
+  },
+  {
+    value: "government",
+    label: "government",
+  },
+  {
+    value: "security",
+    label: "security",
+  },
+  {
+    value: "software",
+    label: "software",
+  },
+  {
+    value: "building",
+    label: "building",
+  },
+  {
+    value: "transportation",
+    label: "transportation",
+  },
+  {
+    value: "wealth",
+    label: "wealth",
+  },
+  {
+    value: "clothing",
+    label: "clothing",
+  },
+  {
+    value: "shortage",
+    label: "shortage",
+  },
+  {
+    value: "debt",
+    label: "debt",
+  },
+  {
+    value: "agriculture",
+    label: "agriculture",
+  },
+  {
+    value: "tax",
+    label: "tax",
+  },
+  {
+    value: "carbon",
+    label: "carbon",
+  },
+  {
+    value: "brexit",
+    label: "brexit",
+  },
+  {
+    value: "workforce",
+    label: "workforce",
+  },
+  {
+    value: "change",
+    label: "change",
+  },
+  {
+    value: "automaker",
+    label: "automaker",
+  },
+  {
+    value: "nuclear",
+    label: "nuclear",
+  },
+  {
+    value: "3D",
+    label: "3D",
+  },
+  {
+    value: "water",
+    label: "water",
+  },
+  {
+    value: "data",
+    label: "data",
+  },
+  {
+    value: "fossil fuel",
+    label: "fossil fuel",
+  },
+  {
+    value: "election",
+    label: "election",
+  },
+  {
+    value: "greenhouse gas",
+    label: "greenhouse gas",
+  },
+  {
+    value: "information",
+    label: "information",
+  },
+  {
+    value: "shale gas",
+    label: "shale gas",
+  },
+  {
+    value: "factory",
+    label: "factory",
+  },
+  {
+    value: "farm",
+    label: "farm",
+  },
+  {
+    value: "communication",
+    label: "communication",
+  },
+  {
+    value: "storm",
+    label: "storm",
+  },
+  {
+    value: "consumer",
+    label: "consumer",
+  },
+  {
+    value: "material",
+    label: "material",
+  },
+  {
+    value: "Washington",
+    label: "Washington",
+  },
+  {
+    value: "pollution",
+    label: "pollution",
+  },
+  {
+    value: "fracking",
+    label: "fracking",
+  },
 ];
 
 export const sourceData = [
-  "EIA",
-  "sustainablebrands.com",
-  "SBWire",
-  "CleanTechnica",
-  "TRAC News",
-  "Vanguard News",
-  "Avi Melamed",
-  "WSJ",
-  "Abq",
-  "Reuters",
-  "Star Tribune",
-  "EV Obsession",
-  "creamermedia",
-  "Resilience",
-  "TheNews.NG",
-  "FashionNetwork.com",
-  "Bloomberg Business",
-  "Yes Bank",
-  "EGYPS",
-  "marketrealist",
-  "PDQFX news",
-  "therobotreport",
-  "nextbigfuture",
-  "World Bank",
-  "Zero Hedge",
-  "Rigzone",
-  "International Business Times",
-  "DOE EIA 2013 Energy Conference",
-  "AllAfrica",
-  "Energy.gov Website",
-  "AMERICAN COUNCIL ON SCIENCE AND HEALTH",
-  "The Jakarta Post",
-  "Wharton",
-  "African Arguments",
-  "Business Insider",
-  "Convenience Store Decisions",
-  "The Next Web",
-  "Cii Radio",
-  "Global Money Trends",
-  "Guardian Sustainable Business",
-  "OklahomaMinerals.com",
-  "4th Annual Congress and Expo on Biofuels and Bioenergy April 27-28 2017 Dubai UAE",
-  "FX Empire",
-  "Nexus Conference",
-  "Fews Net",
-  "Sputnik News",
-  "platts",
-  "CBO",
-  "The Chirographer",
-  "THE LEAGUE OF WOMEN VOTERS® OF THE FAIRFAX AREA",
-  "Yahoo Finance Canada",
-  "Gii Research",
+  {
+    value: "EIA",
+    label: "EIA",
+  },
+  {
+    value: "sustainablebrands.com",
+    label: "sustainablebrands.com",
+  },
+  {
+    value: "SBWire",
+    label: "SBWire",
+  },
+  {
+    value: "CleanTechnica",
+    label: "CleanTechnica",
+  },
+  {
+    value: "TRAC News",
+    label: "TRAC News",
+  },
+  {
+    value: "Vanguard News",
+    label: "Vanguard News",
+  },
+  {
+    value: "Avi Melamed",
+    label: "Avi Melamed",
+  },
+  {
+    value: "WSJ",
+    label: "WSJ",
+  },
+  {
+    value: "Abq",
+    label: "Abq",
+  },
+  {
+    value: "Reuters",
+    label: "Reuters",
+  },
+  {
+    value: "Star Tribune",
+    label: "Star Tribune",
+  },
+  {
+    value: "EV Obsession",
+    label: "EV Obsession",
+  },
+  {
+    value: "creamermedia",
+    label: "creamermedia",
+  },
+  {
+    value: "Resilience",
+    label: "Resilience",
+  },
+  {
+    value: "TheNews.NG",
+    label: "TheNews.NG",
+  },
+  {
+    value: "FashionNetwork.com",
+    label: "FashionNetwork.com",
+  },
+  {
+    value: "Bloomberg Business",
+    label: "Bloomberg Business",
+  },
+  {
+    value: "Yes Bank",
+    label: "Yes Bank",
+  },
+  {
+    value: "EGYPS",
+    label: "EGYPS",
+  },
+  {
+    value: "marketrealist",
+    label: "marketrealist",
+  },
+  {
+    value: "PDQFX news",
+    label: "PDQFX news",
+  },
+  {
+    value: "therobotreport",
+    label: "therobotreport",
+  },
+  {
+    value: "nextbigfuture",
+    label: "nextbigfuture",
+  },
+  {
+    value: "World Bank",
+    label: "World Bank",
+  },
+  {
+    value: "Zero Hedge",
+    label: "Zero Hedge",
+  },
+  {
+    value: "Rigzone",
+    label: "Rigzone",
+  },
+  {
+    value: "International Business Times",
+    label: "International Business Times",
+  },
+  {
+    value: "DOE EIA 2013 Energy Conference",
+    label: "DOE EIA 2013 Energy Conference",
+  },
+  {
+    value: "AllAfrica",
+    label: "AllAfrica",
+  },
+  {
+    value: "Energy.gov Website",
+    label: "Energy.gov Website",
+  },
+  {
+    value: "AMERICAN COUNCIL ON SCIENCE AND HEALTH",
+    label: "AMERICAN COUNCIL ON SCIENCE AND HEALTH",
+  },
+  {
+    value: "The Jakarta Post",
+    label: "The Jakarta Post",
+  },
+  {
+    value: "Wharton",
+    label: "Wharton",
+  },
+  {
+    value: "African Arguments",
+    label: "African Arguments",
+  },
+  {
+    value: "Business Insider",
+    label: "Business Insider",
+  },
+  {
+    value: "Convenience Store Decisions",
+    label: "Convenience Store Decisions",
+  },
+  {
+    value: "The Next Web",
+    label: "The Next Web",
+  },
+  {
+    value: "Cii Radio",
+    label: "Cii Radio",
+  },
+  {
+    value: "Global Money Trends",
+    label: "Global Money Trends",
+  },
+  {
+    value: "Guardian Sustainable Business",
+    label: "Guardian Sustainable Business",
+  },
+  {
+    value: "OklahomaMinerals.com",
+    label: "OklahomaMinerals.com",
+  },
+  {
+    value:
+      "4th Annual Congress and Expo on Biofuels and Bioenergy April 27-28 2017 Dubai UAE",
+    label:
+      "4th Annual Congress and Expo on Biofuels and Bioenergy April 27-28 2017 Dubai UAE",
+  },
+  {
+    value: "FX Empire",
+    label: "FX Empire",
+  },
+  {
+    value: "Nexus Conference",
+    label: "Nexus Conference",
+  },
+  {
+    value: "Fews Net",
+    label: "Fews Net",
+  },
+  {
+    value: "Sputnik News",
+    label: "Sputnik News",
+  },
+  {
+    value: "platts",
+    label: "platts",
+  },
+  {
+    value: "CBO",
+    label: "CBO",
+  },
+  {
+    value: "The Chirographer",
+    label: "The Chirographer",
+  },
+  {
+    value: "THE LEAGUE OF WOMEN VOTERS® OF THE FAIRFAX AREA",
+    label: "THE LEAGUE OF WOMEN VOTERS® OF THE FAIRFAX AREA",
+  },
+  {
+    value: "Yahoo Finance Canada",
+    label: "Yahoo Finance Canada",
+  },
+  {
+    value: "Gii Research",
+    label: "Gii Research",
+  },
 ];
