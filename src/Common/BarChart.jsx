59aafa9fb066e8c4f94599e7bdc49fcb4ed9e92a
AdilAhmed976 <adilahmedah976@gmail.com>
2023-11-22 17:21:20 +0530

    Gradient added on bar chart

--- src/Common/BarChart.jsx
+++ src/Common/BarChart.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 
 const BarChart = ({ data, name = "Pass Name Props" }) => {
+  // Helper function to create a linear gradient fill
+  const createGradientFill = (color1, color2) => {
+    const ctx = document.createElement("canvas").getContext("2d");
+    const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    gradient.addColorStop(0, color1);
+    gradient.addColorStop(1, color2);
+    return gradient;
+  };
+
+  const gradientFill = createGradientFill("#7367f0", "#26d6eb");
+
   const chartData = {
-    labels: data?.map((e, i) => name + "-" + (i + 1)),
+    labels: data,
     datasets: [
       {
         label: name,
         data: data,
-        backgroundColor: "rgba(75, 192, 192, 0.5)",
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 2,
-        borderRadius: 15, // Set border radius for all bars
+        // backgroundColor: "rgba(75, 192, 192, 0.5)",
+        backgroundColor: gradientFill,
+        // borderColor: "rgba(75, 192, 192, 1)",
+        // borderWidth: 2,
+        borderRadius: 25, // Set border radius for all bars
       },
     ],
   };
@@ -19,26 +31,30 @@ const BarChart = ({ data, name = "Pass Name Props" }) => {
   const options = {
     plugins: {
       legend: {
-        display: false, // Set to false to hide the legend
+        display: true, // Set to false to hide the legend
       },
     },
-    // scales: {
-    //   x: {
-    //     grid: {
-    //       display: false,
-    //     },
-    //   },
-    //   y: {
-    //     beginAtZero: true,
-    //     grid: {
-    //       display: false,
-    //     },
-    //   },
-    // },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: true,
+        },
+      },
+    },
+    indexAxis: 'y',
   };
 
   return (
-    <div className="max-w-md mx-auto mt-8">
+    <div
+      // className="max-w-md mx-auto mt-8"
+      className="w-full"
+    >
       <Bar data={chartData} options={options} />
     </div>
   );
