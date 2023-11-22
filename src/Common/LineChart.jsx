7bfc1d1511f22a62377277a3c1b822e950c5b57e
AdilAhmed976 <adilahmedah976@gmail.com>
2023-11-22 15:38:53 +0530

    gardient added in line cahrt

--- src/Common/LineChart.jsx
+++ src/Common/LineChart.jsx
@@ -2,20 +2,33 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 
 const LineChart = ({ data, name = "pass Label" }) => {
+
+  // Helper function to create a linear gradient fill
+const createGradientFill = (color1, color2) => {
+  const ctx = document.createElement('canvas').getContext('2d');
+  const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+  gradient.addColorStop(0, color1);
+  gradient.addColorStop(1, color2);
+  return gradient;
+};
+
+  const gradientFill = createGradientFill("#7367f0", 'rgba(255, 255, 255, 0)');
+
   const chartData = {
     labels: data?.map((e, i) => name + "-" + (i + 1)),
     datasets: [
       {
         label: name,
-        data: data,
-        fill: false,
-        // borderColor: "rgb(75, 192, 192)",
+        data: data || [],
+        borderWidth: 2,
+        fill: true, // Enable fill for the area under the line
+        backgroundColor: gradientFill, // Use the gradient as the fill color
         tension: 0.1,
-        borderColor: "rgb(75, 192, 192)",
-      backgroundColor: "rgb(75, 192, 192)",
+        borderColor: "#7367f0",
+      // backgroundColor: "rgb(75, 192, 192)",
       pointStyle: 'circle',
-      pointRadius: 4,
-      pointHoverRadius: 10
+      pointRadius: 6,
+      pointHoverRadius: 14
       },
     ],
   };
@@ -24,9 +37,20 @@ const LineChart = ({ data, name = "pass Label" }) => {
     scales: {
       x: {
         display: false, // Set to false to hide the x-axis labels
+        ticks: {
+          // color: 'red', // Change x-axis label text color
+          // font: {
+          //   size: 14, // Change x-axis label text size
+          // },
+        },
       },
       y: {
         display: true, // Set to false to hide the y-axis labels
+        ticks: {
+          font: {
+            size: 16, // Change x-axis label text size
+          },
+        },
       },
     },
     plugins: {
@@ -37,7 +61,7 @@ const LineChart = ({ data, name = "pass Label" }) => {
   };
 
   return (
-    <div className="border-2 flex jutsify-center items-center">
+    <div className="flex jutsify-center items-center text-white">
       <Line data={chartData} options={options}  />
     </div>
   );
